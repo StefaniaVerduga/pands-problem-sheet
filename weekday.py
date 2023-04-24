5e93a5c8744ac048c4e618e57ec011b888e75759
StefaniaVerduga <g00425680@atu.ie>
2023-04-24 22:02:41 +0100

    updating commits

--- weekday.py
+++ weekday.py
@@ -4,10 +4,9 @@
 
 import datetime
 
-# We need to get the current date and time
+# Need to get the current date and time
 x = datetime.datetime.now()
-# By default, these calendars have Monday as the first day of the week
-# and Sunday as the last day (Monday = 0 and Sunday = 6)
+# Monday = 0 and Sunday = 6
 if x.weekday() < 5:
     print("Yes, unfortunately today is a weekday.")
 else:
