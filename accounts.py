523d96d7ed5ae07dea9095e52f277dba2e23c7e6
StefaniaVerduga <g00425680@atu.ie>
2023-04-24 22:06:44 +0100

    last changes

--- accounts.py
+++ accounts.py
@@ -3,15 +3,12 @@
 # This program reads in a 10 character account number
 # and outputs the account number with only the last 4 digits showing
 
-# Read in account number
 account_num = input("Please enter an account number: ")
 
-# Get the length of the account number
 num_digits = len(account_num)
 
 # Depending on the account length, the four last digits will be displayed,
-# and we will replace the other digits with 'x'
+# and the other digits will be replaced with 'x'
 new_account_num = 'X' * (num_digits - 4) + account_num[-4:]
 
-# Output the new account number
 print(new_account_num)
